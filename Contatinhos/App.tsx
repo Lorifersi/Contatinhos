7bc4daf792d6252d8600fe8112d7b8eae274ef7a
Lorifersi <lorenafersilva17@gmail>
2024-09-05 14:58:45 -0300

    Estilizado a página

--- Contatinhos/App.tsx
+++ Contatinhos/App.tsx
@@ -1,3 +1,5 @@
+import { StatusBar } from 'react-native'
+
 import {
   useFonts,
   Ubuntu_700Bold,
@@ -18,4 +20,14 @@ export default function App() {
   if(!fontsLoaded){
     return <Loading />
   }
+
+
+return(
+  <>
+  <StatusBar barStyle={'light-content'}
+  backgroundColor={"transparent"}
+  translucent/>
+  <Home/>
+  </>
+)
 }
